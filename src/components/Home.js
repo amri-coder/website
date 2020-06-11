4325274893a853f384dd54e8b3e0398fbb97b3a5
amri <amri.maher@yahoo.com>
2020-06-11 14:39:04 +0200

    projet

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import "../css/home.css";
 
+
+import RobotAnimated from "./robot-animated"
+
 const Home = () => (
     
          
         
 <div className="home">
+    <RobotAnimated />
     <ReactCSSTransitionGroup transitionName="anim" transitionAppear={true} transitionAppearTimeout={5000} transitionEnter={true} transitionLeave={false}>
         <h2>{'AMRI Maher - Ingénieur d\'étude et développement'}</h2>
     </ReactCSSTransitionGroup>
